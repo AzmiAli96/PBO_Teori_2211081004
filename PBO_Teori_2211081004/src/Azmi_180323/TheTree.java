3121492c45ac04f8d171419c34196371e38a5ab5
AzmiAli96 <azmialid@gmail.com>
2023-03-23 22:54:40 +0700

    180323

---
+++ PBO_Teori_2211081004/src/Azmi_180323/TheTree.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Azmi_180323;
+
+/**
+ *
+ * @author nitro
+ */
+public class TheTree {
+    public static void main (String []args){
+        System.out.print("I think that I shall never see,\n");
+        System.out.print("a poem as lovely as a tree.\n");
+        System.out.print("A tree whose hungry mouth is pressed\n");
+        System.out.print("Against the Earth’s sweet flowing breast.\n");
+    }
+}
---
+++ PBO_Teori_2211081004/src/Azmi_180323/azmiali.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Azmi_180323;
+
+/**
+ *
+ * @author nitro
+ */
+public class azmiali {
+    public static void main (String [] args){
+        String nama = "Azmi Ali";
+        System.out.println("Welcome to java programing "+nama);
+    }
+    
+}
